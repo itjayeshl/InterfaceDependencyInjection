8d5dd6f7095b1843c7fc3c97c737d7793765b48c
Jayesh L Lolariya <it.jayeshl@gmail.com>
2017-07-26 23:40:18 +0530

    Initail Commit

---
+++ DI-Interface/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InterfaceInjection;
+namespace DI_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ImplementResult obj = new ImplementResult();
+            string resultValue = obj.getResult();
+            string resultProduct = obj.getResultFromProduct();
+        }
+    }
+}
---
+++ InterfaceInjection/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceInjection
+{
+    public class Class1
+    {
+    }
+
+    interface IBusinessLogic
+    {
+        //Some code
+        string setSomeValue();
+    }
+
+    public class ProductBL : IBusinessLogic
+    {
+        //Some code
+        public string setSomeValue()
+        {
+            return "return from Product BL";
+        }
+    }
+
+    public class CustomerBL : IBusinessLogic
+    {
+        public string setSomeValue()
+        {
+            return "return from Customer BL";
+        }
+        //Some code
+    }
+    interface IBusinessFacade
+    {
+        //void SetBLObject(IBusinessLogic objbusinessLogic);
+    }
+    class BusinessFacade : IBusinessFacade
+    {
+        IBusinessLogic _businessLogic;
+        public void SetBLObject(IBusinessLogic businessLogic)
+        {
+            this._businessLogic = businessLogic;
+        }
+    }
+    public class ImplementResult
+    {
+        public string getResult()
+        {
+            IBusinessLogic businessLogic = new CustomerBL();
+            BusinessFacade businessFacade = new BusinessFacade();
+
+            businessFacade.SetBLObject(businessLogic);
+            return businessLogic.setSomeValue();
+        }
+        public string getResultFromProduct()
+        {
+            IBusinessLogic businessLogic = new ProductBL();
+            BusinessFacade businessFacade = new BusinessFacade();
+
+            businessFacade.SetBLObject(businessLogic);
+            return businessLogic.setSomeValue();
+        }
+    }
+}
